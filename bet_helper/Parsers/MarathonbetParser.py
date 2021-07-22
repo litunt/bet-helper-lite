b9a7f453e8a86061835b7b988c27768f7ea274c5
kodone <kodone@ttu.ee>
2021-07-22 19:40:12 +0200

    marathon parser's returns 2 field instead of list

--- bet_helper/Parsers/MarathonbetParser.py
+++ bet_helper/Parsers/MarathonbetParser.py
@@ -19,7 +19,7 @@ class MarathonbetParser(AbstractParser):
 
             away_team_name = founded_tags[0].text.replace('\n', '')
             home_team_name = founded_tags[1].text.replace('\n', '')
-            return [home_team_name, away_team_name]
+            return home_team_name, away_team_name
         except IndexError:
             class_attributes = 'member-name nowrap'
             founded_tags = soup.find_all(tag, class_=class_attributes)
